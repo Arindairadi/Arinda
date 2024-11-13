802e8daf780a7335dac7c1853b84486c9ccaf902
Arinda Iradi <151714474+Arindairadi@users.noreply.github.com>
2024-11-13 07:22:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
     <title>Attendance Management</title>
 </head>
-<body bgcolour="ABD055">
+<body bgcolor="ABD055">
     <h1>Automated Attendance Management System</h1>
     <a href="attendance.php">View Attendance</a>
     <a href="Add attendance.php">Add Attendance</a>
